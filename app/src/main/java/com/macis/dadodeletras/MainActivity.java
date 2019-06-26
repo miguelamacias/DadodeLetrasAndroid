6a7663c8152a28885783fbe4396f1b33a81572d0
Macías <miguel.a.macias.13@gmail.com>
2019-06-26 15:16:30 +0200

    TextView sustituido por un toast.

--- app/src/main/java/com/macis/dadodeletras/MainActivity.java
+++ app/src/main/java/com/macis/dadodeletras/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     //Crear el dado
@@ -19,7 +19,6 @@ public class MainActivity extends Activity {
     }
 
     public void tirarDado(View view) {
-        TextView resultado = (TextView) findViewById(R.id.resultado);
         ImageView cuadroImagen = (ImageView) findViewById(R.id.imagenLetra);
 
         if (dado.getCantidadLetrasJugadas() < dado.getCantidadLetrasTotal()) {
@@ -27,10 +26,11 @@ public class MainActivity extends Activity {
             String letra = String.format("%c", dado.siguienteLetra());
             int imagen = this.getResources().getIdentifier(letra, "drawable", this.getPackageName());
             cuadroImagen.setImageResource(imagen);
-            resultado.setText(getString(R.string.vacio));
 
         } else {
-            resultado.setText(getString(R.string.finLetras));
+            Toast mensaje = Toast.makeText(this, R.string.finLetras, Toast.LENGTH_LONG);
+            mensaje.show();
+
             cuadroImagen.setImageResource(R.drawable.abecedario);
             dado.restablecerLetrasUsadas();
 
